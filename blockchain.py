087dc50699a64761b8802122015c4e084a5dec8d
araju8844 <araju8844@gmail.com>
2020-06-28 00:30:19 -0400

    completed hash and last_block functions

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,12 @@
+from time import time
+import json
+import hashlib
+
+from urllib.parse import urlparse
+
+import requests
+
+
 class Blockchain:
     def __init__(self):
         self.chain = [] #normally a linked list but simplified to a list here
@@ -6,7 +15,7 @@ class Blockchain:
 
         #create the first block of the chain
         self.new_block(previous_hash = '1', proof = 100)
-        
+
 
     #function to register a neighboring node (other user) in personal address book
 
@@ -15,13 +24,21 @@ class Blockchain:
     #function to resolve conflicts in between chains
 
     #function to create a new block
-
+    def new_block(self,previous_hash,proof):
+        pass
     #function to create a new sale/transaction of money
 
     #function to get the most recent block of the blockchain
-
+    @property
+    def last_block(self):
+        return self.chain[-1]
     #function to create a cryptographic hash
-
+    @staticmethod
+    def hash(block):
+        #put all JSON metadata into encoded string
+        strBlock = json.dumps(block, sort_keys = True).encode()
+        #return SHA256 has in hex
+        return hashlib.sha256(strBlock).hexdigest()
     #function to check the proof of work of a block
 
     #function to check if a proof of work is valid
